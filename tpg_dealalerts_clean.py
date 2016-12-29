d3090b920c074bae51c5637038540374944ac1cc
Sean Bailey - Work <sean.bailey@exeloncorp.com>
2016-12-29 13:23:15 -0500

    split db stuff out into functions

--- tpg_dealalerts_clean.py
+++ tpg_dealalerts_clean.py
@@ -45,21 +45,23 @@ def text_parse(text):
         return False
 
 def email_deal(text):
-    server = smtplib.SMTP(get_config_item('email', 'server'), int(get_config_item('email', 'port')))
-    user = get_config_item('email', 'user')
-    pw = get_config_item('email', 'pass')
-    recipients = get_config_item('email', 'recipients')
+    if args.email:
+        server = smtplib.SMTP(get_config_item('email', 'server'), int(get_config_item('email', 'port')))
+        user = get_config_item('email', 'user')
+        pw = get_config_item('email', 'pass')
+        recipients = get_config_item('email', 'recipients')
     
-    msg = MIMEText(text.encode("utf-8"))
-    msg['Subject'] = 'SB test: Airfare Deal Found!'
-    msg['From'] = user
-    #msg['To'] = ','.join(recipients)
-    msg['To'] = recipients
+        msg = MIMEText(text.encode("utf-8"))
+        msg['Subject'] = 'SB test: Airfare Deal Found!'
+        msg['From'] = user
+        #msg['To'] = ','.join(recipients)
+        msg['To'] = recipients
     
-    server.starttls()
-    server.login(user, pw)
-    server.sendmail(user, recipients, msg.as_string())
-    server.quit
+        print 'sending email to:', recipients
+        server.starttls()
+        server.login(user, pw)
+        server.sendmail(user, recipients, msg.as_string())
+        server.quit
 
 def write_csv(screen_name, tweets):
     with open('%s_tweets.csv' % screen_name, 'wb') as f:
@@ -83,61 +85,55 @@ def get_config_item(section, item):
     ret = config[section].get(item)
     return ret
 
-def main(screen_name):
+def load_tweetdb():
     #initialize list to hold tweets as dicts
     tweetdb = []
-    
     if args.db:
         #check to see if file already exists
         print 'db arg is:', args.db
         if os.path.isfile(args.db):
+            print ''
             with open(args.db, 'r') as db:
                 #load tweets as a list of dictionaries
                 tweetdb = json.load(db)
-                print tweetdb
-                for tweet in tweetdb:
-                    print type(tweet)
-                    print tweet['id']
-                    print sorted(tweet.keys())
+    else:
+        print args.db, 'is empty.'
+    return tweetdb
+
+def process_tweetdb(tweetdb, tweet):
+    if args.db:
+        if len(tweetdb) > 0:
+            if any(item['id'] == tweet.id for item in tweetdb):
+                print '!!!TWEET ALREADY IN DB!!!'
+            else:
+                print 'new tweet! adding it. id:', tweet.id
+                tweetdb.append(tweet._json)
         else:
-            print args.db, 'is empty.'
-                
+            #if dealing with empty database, immediately add tweet
+            print 'empty database. adding tweet. id:', tweet.id
+            tweetdb.append(tweet._json)
+    else:
+        print 'no database used'
+
+def main(screen_name):
+    tweetdb = load_tweetdb()
+    
     api = twitter_auth()
     
-    new_tweets = api.user_timeline(screen_name = screen_name,count=100)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
     
     #parse out tweets not containing special text
     for tweet in new_tweets:
         if text_parse(tweet.text):
             print 'deal found!\n', 'created:', tweet.created_at, '\n', tweet.text, '\n'
-            #email_deal(tweet.text)
-            if 'nyc' in tweet.text.lower() or 'hong kong' in tweet.text.lower():
-                print 'nyc or hong kong found!'
-                #print 'full raw tweet text is:\n', json.dumps(tweet._json, sort_keys = True, indent = 4)
-                if args.db:
-                    if len(tweetdb) > 0:
-                        if any(item['id'] == tweet.id for item in tweetdb):
-                            print '!!!TWEET ALREADY IN DB!!!'
-                        else:
-                            print 'new tweet! adding it. id:', tweet.id
-                            tweetdb.append(tweet._json)
-                        # for item in tweetdb:
-                        #     #print 'item id is:', item['id']
-                        #     #print 'tweet id is:', tweet.id
-                        #     if item['id'] is tweet.id:
-                        #         print 'tweet previously processed!'
-                        #     else:
-                        #         print 'new tweet! adding it. id:', tweet.id
-                        #         tweetdb.append(tweet._json)
-                    else:
-                        #if dealing with empty database, immediately add tweet
-                        print 'empty database. adding tweet. id:', tweet.id
-                        tweetdb.append(tweet._json)
-                if args.email:
-                    print 'sending email to:', get_config_item('email', 'recipients')
-                    email_deal(tweet.text)
+            process_tweetdb(tweetdb, tweet)
+            email_deal(tweet.text)
+    
+    #write changes to database
     with open(args.db, 'w') as db:
         json.dump(tweetdb, db, indent = 4)
+    
+    #quickly check contents of tweetdb
     for item in tweetdb:
         print item['id']
 
